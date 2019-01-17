9813ef44ac077a7a3cd25e4deae4ed455d2b9b02
tiube00001 <408338433@qq.com>
2019-01-17 22:59:53 +0800

    rq

---
+++ js/index.js
@@ -0,0 +1,58 @@
+
+
+
+
+$(function () {
+    // 菜单栏显示子菜单
+    let $li = $(".nav-fl");
+    $li.mouseenter(function () {
+        $(this).children("ul").show();
+    });
+    $li.mouseleave(function () {
+        $(this).children("ul").hide();
+    });
+    // 菜单栏显示子菜单
+
+    // banner区域鼠标移入移出，logo显示圆角矩形条
+
+    let $top_li = $(".top-website>ul>li");
+    $top_li.mouseenter(function () {
+        $(this).children("img").show();
+    });
+    $top_li.mouseleave(function () {
+        $(this).children("img").hide();
+    });
+
+    // banner区域鼠标选中，logo显示圆角矩形条
+
+    $top_li.first(0).children("a").addClass("checked");
+
+    $top_li.each(function(){
+        $(this).click(function(){
+            $top_li.children("a").removeClass("checked");
+            $(this).children("a").addClass("checked");
+
+            // banner轮播图切换
+            let idx = $(this).index();
+            $(".article:eq("+idx+")").show().siblings(".article").hide();
+
+
+        });
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
